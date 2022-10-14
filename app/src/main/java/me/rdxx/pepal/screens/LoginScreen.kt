dab6a1e2fe2313d5f51da1a9619c6c082c1360c2
rdx <31172722+rd-xx@users.noreply.github.com>
2022-10-14 09:53:29 +0200

    add visibility icon to login screen

--- app/src/main/java/me/rdxx/pepal/screens/LoginScreen.kt
+++ app/src/main/java/me/rdxx/pepal/screens/LoginScreen.kt
@@ -19,6 +19,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 
 

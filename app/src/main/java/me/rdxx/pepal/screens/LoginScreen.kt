41fd0928cc2bfaf5afe408eb646d8e2e8f5ea628
rdx <31172722+rd-xx@users.noreply.github.com>
2022-10-14 09:53:45 +0200

    handle physical keyboard ENTER key

--- app/src/main/java/me/rdxx/pepal/screens/LoginScreen.kt
+++ app/src/main/java/me/rdxx/pepal/screens/LoginScreen.kt
@@ -1,14 +1,16 @@
 package me.rdxx.pepal.screens
 
+import android.view.KeyEvent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Visibility
+import androidx.compose.material.icons.rounded.VisibilityOff
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,6 +19,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -29,6 +32,7 @@ import androidx.compose.ui.unit.dp
 fun LoginScreen() {
     var username by rememberSaveable { mutableStateOf("") }
     var password by rememberSaveable { mutableStateOf("") }
+    var passwordVisible by rememberSaveable { mutableStateOf(false) }
 
     val focusManager = LocalFocusManager.current
 
@@ -39,24 +43,50 @@ fun LoginScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         TextField(
-            modifier = Modifier.padding(0.dp, 0.dp, 0.dp, 32.dp),
+            modifier = Modifier
+                .padding(0.dp, 0.dp, 0.dp, 32.dp)
+                .then(Modifier.onKeyEvent {
+                    if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
+                        focusManager.moveFocus(FocusDirection.Down)
+                        true
+                    } else {
+                        false
+                    }
+                }),
             value = username,
             onValueChange = { username = it },
             label = { Text(text = "Nom d'utilisateur") },
             placeholder = { Text(text = "etu.brahimgou") },
             singleLine = true,
             keyboardOptions = KeyboardOptions(
+                autoCorrect = false,
                 imeAction = ImeAction.Next
             ),
-            keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) })
+            keyboardActions = KeyboardActions(onNext = { focusManager.moveFocus(FocusDirection.Down) }),
         )
 
         TextField(
+            modifier = Modifier.onKeyEvent {
+                if (it.nativeKeyEvent.keyCode == KeyEvent.KEYCODE_ENTER) {
+                    focusManager.clearFocus()
+                }
+                true
+            },
             value = password,
             onValueChange = { password = it },
             label = { Text(text = "Mot de passe") },
             placeholder = { Text(text = "Mot de passe") },
             singleLine = true,
+            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+            trailingIcon = {
+                val image =
+                    if (passwordVisible) Icons.Rounded.Visibility else Icons.Rounded.VisibilityOff
+                val description = if (passwordVisible) "Hide password" else "Show password"
+
+                IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                    Icon(imageVector = image, description)
+                }
+            },
             keyboardOptions = KeyboardOptions(
                 imeAction = ImeAction.Done
             ),
